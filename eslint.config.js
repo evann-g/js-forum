726cd68c084dc32737b8064e90c1cad078a2bd7c
Corrailius <louka.jeandaude.self@gmail.com>
2026-05-26 11:32:17 +0200

    feat: remove globals dependency and define Node.js globals explicitly in ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-import globals from "globals";
 import js from "@eslint/js";
 
 /** @type {import("eslint").Linter.Config[]} */
@@ -9,7 +8,24 @@ export default [
       ecmaVersion: 2022,
       sourceType: "module",
       globals: {
-        ...globals.node,
+        // Node.js globals
+        process: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        Buffer: "readonly",
+        console: "readonly",
+        module: "readonly",
+        require: "readonly",
+        exports: "readonly",
+        URL: "readonly",
+        URLSearchParams: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+        setImmediate: "readonly",
+        clearImmediate: "readonly",
+        global: "readonly",
       },
     },
     rules: {
